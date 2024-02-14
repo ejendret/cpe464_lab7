caef083265e28e9142d4de6797223ec9d3020adc
ejendret <ejendret@calpoly.edu>
2024-02-14 13:29:22 -0800

    Create PDU

--- pduUtil.c
+++ pduUtil.c
@@ -6,7 +6,33 @@ int createPDU(uint8_t *pduBuffer, uint32_t sequenceNumber, uint8_t flag, uint8_t
 
     uint32_t netSequenceNumber = htonl(sequenceNumber);
     uint32_t index = 0;
+
+    // Copy network sequence number
     memcpy(pduBuffer + index, &netSequenceNumber, 4);
     index += 4;
+
+    // Skip checksum for now, will compute after
+    int checksumIndex = index;
+    index += 2;
+
+    // Copy flag
+    memcpy(pduBuffer + index, &flag, 1);
+    index += 1;
+
+    // Copy payload
+    memcpy(pduBuffer + index, payload, payloadLen);
+    index += payloadLen;
+
+    // Compute checksum and copy into buffer
+    short checksum = in_cksum(pduBuffer, index);
+    short netChecksum = htons(checksum);
+    memcpy(pduBuffer + checksumIndex, &netChecksum, 2);
+
+    pduLength = index;
+
     return pduLength;
+}
+
+void printPDU(uint8_t *aPDU, int pduLength)
+{
 }
--- pduUtil.h
+++ pduUtil.h
@@ -6,4 +6,6 @@
 // Functions
 //
 int createPDU(uint8_t *pduBuffer, uint32_t sequenceNumber, uint8_t flag, uint8_t *payload, int payloadLen);
+void printPDU(uint8_t *aPDU, int pduLength);
+
 #endif
