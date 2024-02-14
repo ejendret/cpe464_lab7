aeef2e76e1d1d1829d3511bb237263323042ae17
ejendret <ejendret@calpoly.edu>
2024-02-14 14:59:03 -0800

    Forgot to add pduUtil.h change with last commit

--- pduUtil.h
+++ pduUtil.h
@@ -4,8 +4,10 @@
 #include "networks.h"
 #include "checksum.h"
 
+// Defines
+#define HEADER_LEN 7
+
 // Functions
-//
 int createPDU(uint8_t *pduBuffer, uint32_t sequenceNumber, uint8_t flag, uint8_t *payload, int payloadLen);
 void printPDU(uint8_t *aPDU, int pduLength);
 
